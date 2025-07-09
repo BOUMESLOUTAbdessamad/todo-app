7f01fd208ed4714af2db92c78fea2cfb1d919ea3
Abdessamad BOUMESLOUT <abdessamadbm31@gmail.com>
2025-07-09 22:45:22 +0100

    ADD tasks inbox UI

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,8 +1,18 @@
+import {Input} from "@heroui/input";
+import {Checkbox} from "@heroui/checkbox";
+
 function Tasks() {
 
     return (
-        <div>
-            Tasks inbox
+        <div className="container mx-auto p-4 max-w-md">
+
+            <div className="justify-between flex items-center mb-4">
+                <Input type="text" placeholder="+ Add a Task"/>
+            </div>
+            <div className="flex flex-col gap-2">
+                <Checkbox>Meet salim</Checkbox>
+                <Checkbox>Meet salim</Checkbox>
+            </div>
         </div>
     )
 
